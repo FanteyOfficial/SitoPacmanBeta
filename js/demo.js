d5c61076647f383be43ba8f51a83a8a8007d81e9
FanteyOfficial <81305405+FanteyOfficial@users.noreply.github.com>
2022-04-19 02:40:24 +0200

    BigUpdate

---
+++ js/demo.js
@@ -0,0 +1,7 @@
+document.getElementById("backHome").onclick = () => {
+    window.location.href = "../index.html";
+}
+
+document.getElementById("scarica").onclick = () => {
+    window.location.href = "../scarica.html";
+}
---
+++ js/home.js
@@ -0,0 +1,7 @@
+document.getElementById("giocaLaDemo").onclick = () => {
+    window.location.href = "../demo.html";
+}
+
+document.getElementById("scarica").onclick = () => {
+    window.location.href = "../scarica.html";
+}
