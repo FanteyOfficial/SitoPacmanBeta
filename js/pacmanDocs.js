0277a2fdfe35307cd52f377a654cc5f74461316c
FanteyOfficial <81305405+FanteyOfficial@users.noreply.github.com>
2022-04-25 02:15:30 +0200

    Update7

---
+++ js/pacmanDocs.js
@@ -0,0 +1,11 @@
+document.getElementById("giocaLaDemo").onclick = () => {
+    window.location.href = "../demo.html";
+}
+
+document.getElementById("scarica").onclick = () => {
+    window.location.href = "../scarica.html";
+}
+
+document.getElementById("backHome").onclick = () => {
+    window.location.href = "../index.html";
+}
